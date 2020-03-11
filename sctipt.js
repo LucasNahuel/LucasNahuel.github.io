9623a4f473a04e324a37478da31e16670fed0234
Lucas Nahuel Muñoz <munozlucas96@gmail.com>
2020-03-11 18:00:16 -0300

    primer commit

---
+++ sctipt.js
@@ -0,0 +1,20 @@
+document.getElementById("boton-info").onclick = function(){
+    window.scrollTo({
+    top:0,
+    left:0,
+    behavior: 'smooth'})
+}
+
+document.getElementById("boton-formacion").onclick = function(){
+    window.scrollTo({
+    top:620,
+    left:0,
+    behavior: 'smooth'})
+}
+
+document.getElementById("boton-proyectos").onclick = function(){
+    window.scrollTo({
+    top:1080,
+    left:0,
+    behavior: 'smooth'})
+}
